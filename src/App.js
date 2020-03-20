e2b049d8ddd9f08662f085bbb0899e119959d080
Carlos <rick_teloon@hotmail.com>
2020-03-20 20:08:54 -0300

    Ignore push

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/login/LoginWeb';
+
 import 'bootstrap/dist/css/bootstrap.css';
 
 
-function App() {
+function App() {  
   return (
     <div className="App">
         <Login/>
+
     </div>
   );
 }
--- src/components/login/LoginWeb.js
+++ src/components/login/LoginWeb.js
@@ -1,6 +1,8 @@
 import React,{Component} from 'react'
+import Botao from '../botao'
 
 class Login extends Component{
+    
     render(){
          return(
              <div className="container bg-light mt-5">
@@ -22,6 +24,7 @@ class Login extends Component{
                                     <input type="text" className="form-control rounded-pill"/>
                                 </div>
                             </div>
+                            <Botao value="Testando" color="bg-primary"/>
                         </form>
                     </div>
                     <div className="col-lg-8 bg-primary">2</div>
