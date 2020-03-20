e1d9252e863a22aab014ee021c6361687c147f8b
LazaroC <lazaro.oliveiras@hotmail.com>
2020-03-20 16:58:17 -0300

    Adicionando o componente da imagem Toth

---
+++ front-toth/src/components/imagemToth.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class imagemToth extends Component {
+
+    render() {
+        return(
+            <img className=".img-fluid" src=""/>
+        )
+    }
+
+}
