9f2beefb2cce8ccee1be1d4d829bf03ee9d6ea77
Carlos <rick_teloon@hotmail.com>
2020-03-25 16:02:58 -0300

    Corrigindo tamanho do input

--- src/components/cadastro/stepsCadastro/step1.js
+++ src/components/cadastro/stepsCadastro/step1.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react'
 import Botao from '../../botao'
+import Input from '../../inputs'
 
 class Step1 extends Component{
 
@@ -12,8 +13,21 @@ class Step1 extends Component{
 
     render(){
         return(
-            <div className="container bg-dark">
-                    aaa
+            <div className="container bg-light p-2 pr-4 pl-4 pt-5 pb-5">
+                <div className="row">
+                    <Input label="Nome da instituição"></Input>
+                </div>
+                <div className="row">
+                    CNPJ
+                </div>
+                <div className="row">
+                    <input type="text" name="login" className="form-control "/>
+                </div>
+                  <div className="row">
+                      <div className=" ml-auto mt-5 col-2">
+                        <Botao value="Proximo" color=" btn-primary rounded-pill"/>
+                      </div>
+                </div>
             </div>
         )
     }
--- src/components/inputs.js
+++ src/components/inputs.js
@@ -4,9 +4,9 @@ class Inputs extends Component {
 
     render() {
         return(
-            <div className="form-group">
+            <div className="form-group col-12">
                 <label>{this.props.label}</label>
-                <input type={this.props.type} name={this.props.name} onChange={this.capturarDadosCampos} className="form-control rounded-pill"/>
+                <input type={this.props.type} name={this.props.name} onChange={this.capturarDadosCampos} className="form-control"/>
             </div>
         )
     }
