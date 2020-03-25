e82f5921ce4b26cff741f3baa05c022bdfddc3a7
Carlos <rick_teloon@hotmail.com>
2020-03-25 14:20:07 -0300

    Estrutura cadastro

---
+++ src/components/cadastro/Cadastro.js
@@ -0,0 +1,20 @@
+import React,{Component} from 'react'
+import Botao from '../botao'
+
+class Cadastro extends Component{
+
+    constructor(){
+        super()
+
+        this.state = {}
+    }
+
+
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
